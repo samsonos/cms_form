85d6ecaffbd70f671663607e689ed31c2ece395d
Vitaly Egorov <egorov@samsonos.com>
2014-12-11 20:11:17 +0200

    Fixed error

--- src/Tab.php
+++ src/Tab.php
@@ -34,7 +34,10 @@ class Tab extends Container
         // Fire event that tab has been created
         \samson\core\Event::fire('cms_ui.tab_created', array(&$this));
 
-        parent::__construct(($parent = isset($tab) ? $tab : $form));
+        // Define parent container
+        $parent = isset($tab) ? $tab : $form;
+
+        parent::__construct($parent);
     }
 }
 
