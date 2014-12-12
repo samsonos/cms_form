be2d590dd70e0a8867df5512098c6b2190faa162
Vitaly Egorov <egorov@samsonos.com>
2014-12-12 14:41:01 +0200

    Fixed tests

--- tests/FormTest.php
+++ tests/FormTest.php
@@ -13,11 +13,19 @@ class FormTest extends \PHPUnit_Framework_TestCase
     /** @var Form Pointer to form */
     public $form;
 
+    /** @var samson\core\IViewable Mock */
+    public $viewable;
+
     /** Tests init */
     public function setUp()
     {
+        // Create mock
+        $this->viewable = $this->getMockBuilder('samson\core\IViewable')
+            ->disableOriginalConstructor()
+            ->getMock();
+
         // Get instance using services factory as error will signal other way
-        $this->form = new Form();
+        $this->form = new Form($this->viewable);
 
         // Disable default error output
         Error::$OUTPUT = false;
